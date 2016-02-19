292051c3c483f9e90fa7a604aaa835eee35c1449
Jon <lasercar@users.noreply.github.com>
2016-02-19 04:26:20 -0600

    outline basic polygon coordinate formula

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+regular polygon coordinate calculation
+
+http://math.stackexchange.com/questions/1123175
+
+n = number of vertices = number of sides
+a = rotation angle
+r = radius (circumscribed circle)
+
+for each vertex i
+x = r cos(a + 2 i pi / n)
+y = r sin(a + 2 i pi / n)
+
+*/
