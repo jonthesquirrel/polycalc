f244038f171e0d2803cb48a104f6fcafba5af8ef
Jon <lasercar@users.noreply.github.com>
2016-02-19 05:05:22 -0600

    calcPolygon()

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 /*
-regular polygon coordinate calculation
+calculate regular polygon coordinates
 
 http://math.stackexchange.com/questions/1123175
 
 n = number of vertices = number of sides
-a = rotation angle
 r = radius (circumscribed circle)
+a = counterclockwise rotation angle in radians
 
-for each vertex i
+iterate for n (i = current vertex)
 x = r cos(a + 2 i pi / n)
 y = r sin(a + 2 i pi / n)
 
 */
+
+function calcPolygon(input) {
+  var output = []
+  for (var i = 1; i <= input.n; i++) {
+    output.push({
+      x: (input.r * Math.cos(input.a + 2 * i * Math.PI / input.n)),
+      y: (input.r * Math.sin(input.a + 2 * i * Math.PI / input.n))
+    })
+  }
+  return output
+}
